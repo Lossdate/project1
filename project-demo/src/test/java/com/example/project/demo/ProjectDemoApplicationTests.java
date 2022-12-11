9b36381bcb7ba81e63206d476b403e39574f568d
sunyizhen011 <sunyizhen011@zto.com>
2022-12-11 23:54:08 +0800

    new

---
+++ project-demo/src/test/java/com/example/project/demo/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
